f3282dda8bac7e991b793d03dfab2254e38d6ea4
Mohammed Jawad Saleem <mohammedjawadsaleem17@gmail.com>
2025-05-08 20:37:27 +0530

    Added Cors

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 app.use(express.json());
-const PORT = 8080
+const PORT = 8080;
 
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://rshardware-backend.onrender.com/",
+    // Add any other origins you need to allow
+    "https://rshardware.up.railway.app/",
+    "https://rshardware.netlify.app/",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
 
+app.use(cors(corsOptions));
 
-const userRoutes =  require("./routes/userRoutes");
+const userRoutes = require("./routes/userRoutes");
 const { connectDB } = require("./db/db");
 
-
-
 try {
-    connectDB();
+  connectDB();
 } catch (err) {
-    connectDB.connectToDB()
+  connectDB.connectToDB();
 }
 
 app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
-    res.status(200).json({application:"rs-hardware",version:2.1,"health-check":"Success",developer:"2x+1"})
-})
+  res.status(200).json({
+    application: "rs-hardware",
+    version: 2.1,
+    "health-check": "Success",
+    developer: "2x+1",
+  });
+});
+
+
 
 app.listen(PORT, () => {
     console.log("Server Started on PORT: " + "http://localhost:" + PORT);
